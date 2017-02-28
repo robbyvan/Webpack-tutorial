8d578f9e43c41cdf583fc3f9640bd80776ebf641
robbyvan <fzpinus@gmail.com>
2017-02-27 21:44:18 -0800

    use presets

--- Ch02/02_03/start/webpack.config.js
+++ Ch02/02_03/start/webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
-				loader: 'babel'
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
 			}
 		]
 	}
