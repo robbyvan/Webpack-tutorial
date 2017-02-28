5b007a0387d5e3a20763ac7d67ee5a8b33c303b4
robbyvan <fzpinus@gmail.com>
2017-02-27 22:06:37 -0800

    load css with web pack

--- Ch03/03_01/start/src/main.js
+++ Ch03/03_01/start/src/main.js
@@ -1,2 +1,16 @@
-const $ = require('jquery');
-$('#target').html('Hello The World!!!!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./style.css');
+
+
+class Message extends React.Component {
+  render() {
+    return (<div>
+              <h1>{this.props.title}</h1>
+              <p>{this.props.message}</p>
+            </div>);
+  }
+}
+
+ReactDOM.render(<Message title="Email lb" message="Can u email him?" />, 
+  document.getElementById('react-container'));
